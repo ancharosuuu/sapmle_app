a1238ccb5273fea63636f8dffc6a397e23a9e0e0
EC2 Default User <ec2-user@ip-172-31-1-166.ec2.internal>
2020-12-14 04:50:50 +0000

    ９章最後まで

---
+++ db/migrate/20201211141236_add_image_id_to_lists.rb
@@ -0,0 +1,5 @@
+class AddImageIdToLists < ActiveRecord::Migration[5.2]
+  def change
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_113257) do
+ActiveRecord::Schema.define(version: 2020_12_11_141236) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "image_id"
   end
 
 end
